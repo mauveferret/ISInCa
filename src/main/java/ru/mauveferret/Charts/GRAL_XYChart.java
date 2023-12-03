12b53cb593dad8a5a04b3f4766d56dea8dbd9ba5
mauveferret <mauveferret@gmail.com>
2023-12-03 18:10:15 +0300

    changed library for LineCharts to GRAL

--- src/main/java/ru/mauveferret/Charts/GRAL_XYChart.java
+++ src/main/java/ru/mauveferret/Charts/GRAL_XYChart.java
@@ -5,16 +5,23 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 import de.erichseifert.gral.data.DataSeries;
+import de.erichseifert.gral.data.DataSource;
 import de.erichseifert.gral.data.DataTable;
 import de.erichseifert.gral.graphics.Label;
 import de.erichseifert.gral.plots.XYPlot;
 import de.erichseifert.gral.plots.axes.AxisRenderer;
+import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D;
 import de.erichseifert.gral.plots.lines.DiscreteLineRenderer2D;
+import de.erichseifert.gral.plots.lines.LineRenderer;
+import de.erichseifert.gral.plots.points.DefaultPointRenderer2D;
 import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.plots.points.SizeablePointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 import de.erichseifert.gral.util.GraphicsUtils;
 import de.erichseifert.gral.graphics.Insets2D;
+import de.erichseifert.gral.plots.areas.AreaRenderer;
+import de.erichseifert.gral.plots.areas.DefaultAreaRenderer2D;
+import de.erichseifert.gral.plots.areas.LineAreaRenderer2D;
 
 public class GRAL_XYChart extends GRAL_Panel {
 
@@ -51,6 +58,10 @@ public class GRAL_XYChart extends GRAL_Panel {
                 new Color[] { new Color(0, 0, 0, 0), new Color(0, 0, 0, 32), new Color(0, 0, 0, 128) }
         ));
 
+
+        formatFilledArea(plot, seriesLin, new Color(0, 255, 0, 128));
+
+
         // Format axes
         AxisRenderer axisRendererX = plot.getAxisRenderer(XYPlot.AXIS_X);
         AxisRenderer axisRendererY = plot.getAxisRenderer(XYPlot.AXIS_Y);
@@ -64,7 +75,6 @@ public class GRAL_XYChart extends GRAL_Panel {
         axisRendererX.setLabel(linearAxisLabelX);
         axisRendererX.setTickFont(new Font ("TimesRoman", Font.PLAIN, 16));
 
-
         Label linearAxisLabelY = new Label("Intensity, particles/dE");
         linearAxisLabelY.setRotation(90);
         linearAxisLabelY.setFont(new Font ("TimesRoman", Font.PLAIN, 18));
@@ -73,6 +83,8 @@ public class GRAL_XYChart extends GRAL_Panel {
         axisRendererY.setShapeStroke(new BasicStroke(3f));
         axisRendererY.setLabel(linearAxisLabelY);
         axisRendererX.setTickFont(new Font ("TimesRoman", Font.PLAIN, 16));
+        axisRendererX.setIntersection(-Double.MAX_VALUE);
+        axisRendererY.setIntersection(-Double.MAX_VALUE);
 
         // Format rendering of data points
         PointRenderer sizeablePointRenderer = new SizeablePointRenderer();
@@ -87,8 +99,10 @@ public class GRAL_XYChart extends GRAL_Panel {
                 10.0f, new float[] {3f, 6f}, 0.0f));
         plot.setLineRenderers(seriesLin, discreteRenderer);
 
-        InteractivePanel panel = new InteractivePanel(plot);
 
+
+
+        InteractivePanel panel = new InteractivePanel(plot);
         panel.setPopupMenuEnabled(true);
         panel.setAutoscrolls(true);
         // Add plot to Swing component
@@ -106,4 +120,18 @@ public class GRAL_XYChart extends GRAL_Panel {
         return name;
     }
 
+
+    private static void formatFilledArea(XYPlot plot, DataSource data, Color color) {
+        PointRenderer point = new DefaultPointRenderer2D();
+        point.setColor(color);
+        plot.setPointRenderers(data, point);
+        LineRenderer line = new DefaultLineRenderer2D();
+        line.setColor(color);
+        line.setGap(3.0);
+        line.setGapRounded(true);
+        plot.setLineRenderers(data, line);
+        AreaRenderer area = new DefaultAreaRenderer2D();
+        area.setColor(GraphicsUtils.deriveWithAlpha(color, 64));
+        plot.setAreaRenderers(data, area);
+    }
 }
